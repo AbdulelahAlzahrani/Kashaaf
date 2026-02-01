569d8ef7ba786cebbec4fb1c4098f6f4a174a061
AbdulelahAlzahrani <pleasework730@gmail.com>
2026-02-01 20:10:19 +0300

    Add ScrollToTop component to fix navigation issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-gaming-dark text-white">
         <Routes>
           <Route path="/" element={<Home />} />
